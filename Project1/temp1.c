0410f435961268cdef4b265873bfb757f92520cc
NoName48 <aa_aa_aa_11@naver.com>
2022-02-25 19:53:11 +0900

    Update temp1.c
    tree_dice

--- Project1/temp1.c
+++ Project1/temp1.c
@@ -1,5 +1,33 @@
 #include<stdio.h>
 
 int main() {
-	printf("hello world");
+	int a, b, c;
+	int temp = 0;
+	int result=0;
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (a == b&& b == c) {
+		result = 10000 + a * 1000;
+	}
+	else if (a == b || b == c || c == a ) {
+
+		if (a == b)
+			temp = a;
+		else if (b == c)
+			temp = b;
+		else
+			temp = c;
+		result = 1000 + temp * 100;
+	}
+	else {
+		temp = a;
+		if (temp < b)
+			temp = b;
+		if (temp < c)
+			temp = c;
+		result = temp * 100;
+	}
+
+	printf("%d", result);
 }
