758ffcc41b7c726fde3ec6a6718418a4da90e6d2
NoName48 <aa_aa_aa_11@naver.com>
2022-04-02 23:40:58 +0900

    Create draw_star_10.c

---
+++ Project2/draw_star_10.c
@@ -0,0 +1,22 @@
+//#include<stdio.h>
+//
+//int star(int n);
+//
+//int main() {
+//	
+//	int num = 0;
+//	scanf_s("%d", num);
+//	star(num);
+//
+//}
+//int star(int n) {
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			if (i == 1 && j == 1)printf(" ");
+//			else printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 
+//}
