0a6008e71c67102ae8994c7c8e7ea8616205a3ab
NoName48 <aa_aa_aa_11@naver.com>
2022-04-08 23:55:44 +0900

    Create tmp3.c

---
+++ Project2/tmp3.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//
+//int str_len(char *n,int i) {
+//	if (n[i] != NULL)i++;
+//	else return i;
+//	return str_len(n, i);
+//}
+//
+//int main() {
+//	char u_str[100] = { 0 };
+//	int i = 0;
+//	scanf_s("%s", &u_str,100);
+//	printf("입력한 문자열의 길이는 %d입니다.", str_len(u_str,i));
+//}
