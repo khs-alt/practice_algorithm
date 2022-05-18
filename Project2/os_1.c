ed91b54e59c96871dc80d85f0ee262adff3ebbf3
NoName48 <aa_aa_aa_11@naver.com>
2022-05-18 23:48:31 +0900

    Create os_1.c

---
+++ Project2/os_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int process_num, process_id;
+	int arrival_time, service_time;
+	int buffer[1024];
+		
+	//FILE* fp;
+	//fp = fopen("process_list_5.txt", "r");
+
+	//for(int i = 0; i < process_num; i++)
+	//	fread(buffer, sizeof(int), 1024, fp);
+	
+
+}
