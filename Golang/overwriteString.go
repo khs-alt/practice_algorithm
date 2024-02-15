b10834a07bb41409adb21d0e5f21aa3835aa2f17
khs-alt <aa_aa_aa_11@naver.com>
2024-02-16 02:46:58 +0900

    solved baekjoon q2178 dfs

--- Golang/overwriteString.go
+++ Golang/overwriteString.go
@@ -1,9 +1,11 @@
-func solution(my_string string, overwrite_string string, s int) string {
-    s1 := []byte(my_string)//string is immutable so it chang byte array for mutable 
-    s2 := []byte(overwrite_string)
-    n := len(s2)
-    for i := 0; i < n; i++ {
-        s1[s+i] = s2[i]
-    }
-    return string(s1)
+package main
+
+func Solution(my_string string, overwrite_string string, s int) string {
+	s1 := []byte(my_string) //string is immutable so it chang byte array for mutable
+	s2 := []byte(overwrite_string)
+	n := len(s2)
+	for i := 0; i < n; i++ {
+		s1[s+i] = s2[i]
+	}
+	return string(s1)
 }
--- leetcode_poor_pig.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-        cycle = minutesToTest // minutesToDie + 1
-        pigs = 0
-        while(cycle)**pigs < buckets:
-            pigs+=1
-            
-        return pigs
